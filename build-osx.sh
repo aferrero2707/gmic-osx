99173fafbe880ba0c2980f4f25297fc0b7ad45af
aferrero2707 <aferrero1975@gmail.com>
2018-10-20 18:07:51 +0200

    Update build-osx.sh

--- build-osx.sh
+++ build-osx.sh
@@ -9,31 +9,20 @@ export PKG_CONFIG_PATH="/usr/local/opt/curl/lib/pkgconfig:/usr/local/opt/zlib/li
 export LD_LIBRARY_PATH="/usr/local/opt/curl/lib:/usr/local/opt/zlib/lib:$LD_LIBRARY_PATH"
 
 
-if [ ! -e gmic-qt ]; then
-	echo "Running git clone https://github.com/c-koi/gmic-qt.git"
-	git clone https://github.com/c-koi/gmic-qt.git || exit 1
-	echo "... finished"
-fi
-cd gmic-qt || exit 1
-if [ ! -e gmic-clone ]; then
+if [ ! -e gmic ]; then
 	#echo "Running git clone --depth=1 https://framagit.org/dtschump/gmic.git gmic-clone"
 	#git clone --depth=1 https://framagit.org/dtschump/gmic.git gmic-clone || exit 1
-	echo "Running git clone https://github.com/dtschump/gmic.git gmic-clone"
-	git clone https://github.com/dtschump/gmic.git gmic-clone || exit 1
+	echo "Running git clone https://github.com/dtschump/gmic.git gmic"
+	git clone https://github.com/dtschump/gmic.git gmic || exit 1
 	echo "... finished"
-	patch -p1 < ../../GMIC/gmic-plugins-path.patch
 fi
-
-echo "Generating GMIC headers"
-make -C gmic-clone/src CImg.h gmic_stdlib.h || exit 1
-
 
 export CC="gcc -mmacosx-version-min=10.8 -fno-stack-protector -march=nocona -mno-sse3 -mtune=generic"
 export CXX="g++ -mmacosx-version-min=10.8 -fno-stack-protector -march=nocona -mno-sse3 -mtune=generic"
 
-make -B cli "SUBLIBS=-lX11"
-makedir /tmp/gmic-cli
-cp gmic /tmp/gmic-cli/
+make -B cli "SUBLIBS=-lX11" || exit 1
+mkdir /tmp/gmic-cli || exit 1
+cp -a gmic /tmp/gmic-cli/ || exit 1
 
 cd "$TRAVIS_BUILD_DIR"
 if [ ! -e macdylibbundler ]; then
@@ -42,7 +31,7 @@ if [ ! -e macdylibbundler ]; then
 fi
 
 
-cd /tmp/gmic-cli
+cd /tmp/gmic-cli || exit 1
 "$TRAVIS_BUILD_DIR"/macdylibbundler/dylibbundler -b -od -x gmic -cd -p "@rpath" > /dev/null
 install_name_tool -add_rpath "@loader_path/libs" gmic
 cd …
