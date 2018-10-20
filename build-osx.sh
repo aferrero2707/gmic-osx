cb05ed4c9539ea230a4a2d0ee86a992f8eb87b1b
aferrero2707 <aferrero1975@gmail.com>
2018-10-20 19:49:41 +0200

    Update build-osx.sh

--- build-osx.sh
+++ build-osx.sh
@@ -35,5 +35,5 @@ fi
 cd /tmp/gmic-cli || exit 1
 "$TRAVIS_BUILD_DIR"/macdylibbundler/dylibbundler -b -od -x gmic -cd -p "@rpath" > /dev/null
 install_name_tool -add_rpath "@loader_path/libs" gmic
-cd …
+cd ..
 tar czf "$TRAVIS_BUILD_DIR"/gmic-cli.tgz gmic-cli
