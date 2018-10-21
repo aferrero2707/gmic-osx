c4b3fbe45e617da1b166aac4a7ebf49833e863f2
aferrero2707 <aferrero1975@gmail.com>
2018-10-21 13:10:58 +0200

    Update build-osx.sh

--- build-osx.sh
+++ build-osx.sh
@@ -34,9 +34,10 @@ fi
 
 cd /tmp/gmic-cli || exit 1
 "$TRAVIS_BUILD_DIR"/macdylibbundler/dylibbundler -b -od -x gmic -cd -p "@rpath" > /dev/null
+cp -a /usr/local/lib/libopencv_*.dylib libs
 echo "install_name_tool -add_rpath \"@loader_path/libs\" gmic"
 install_name_tool -add_rpath "@loader_path/libs" gmic
-for F in lib/*.dylib; do
+for F in libs/*.dylib; do
 	echo "install_name_tool -add_rpath \"@loader_path\" \"$F\""
 	install_name_tool -add_rpath "@loader_path" "$F"
 done
